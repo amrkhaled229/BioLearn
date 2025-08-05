c984bcdeaab6ea20fc056daef6806a25d0970dbd
amrkhaled229 <amrkhaledosman2003@gmail.com>
2025-08-05 17:11:35 +0300

    Hide Firebase config using .env.local

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,21 +1,18 @@
 // lib/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZPZPnYD_s2cvqpZsXMV6tLPkz_Oz4tRg",
-  authDomain: "biolearn-f55cb.firebaseapp.com",
-  projectId: "biolearn-f55cb",
-  storageBucket: "biolearn-f55cb.firebasestorage.app",
-  messagingSenderId: "989891806111",
-  appId: "1:989891806111:web:a6a9804439b215f1ed33aa",
-  measurementId: "G-19CZQBXC08"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export the Auth instance
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
